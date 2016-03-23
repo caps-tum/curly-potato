7f49bb3ac54672c515fdb1d631fe03c37f0c3126
Jens Breitbart <jbreitbart@gmail.com>
2016-03-23 15:27:58 +0100

    Replaced fixed space2d with generic N-dimensional space.

--- space.cpp
+++ space.cpp
@@ -4,6 +4,7 @@
 #include <functional>
 #include <iostream>
 #include <tuple>
+#include <type_traits>
 
 #include <cassert>
 
@@ -23,7 +24,7 @@ template <typename spaceT> struct iteration<2, spaceT> {
 	iteration<2, spaceT>() = delete;
 	iteration<2, spaceT>(const iteration<2, spaceT> &) = default;
 
-	iteration<2, spaceT>(spaceT s) : i(s.start_i), j(s.start_j), _space(s) {}
+	iteration<2, spaceT>(spaceT s) : i(s.start[0]), j(s.start[1]), _space(s) {}
 
 	bool operator!=(const iteration<2, spaceT> &rhs) const noexcept {
 		return rhs.i != i || rhs.j != j || rhs._space != _space;
@@ -37,17 +38,32 @@ template <typename spaceT> struct iteration<2, spaceT> {
 	auto operator*() const noexcept { return std::make_tuple(i, j); }
 };
 
-// space1d * space1d = space2d?
-struct space2d {
-	int start_i, end_i;
-	int start_j, end_j;
-	space2d(const int start_i_, const int end_i_, const int start_j_, const int end_j_)
-		: start_i(start_i_), end_i(end_i_), start_j(start_j_), end_j(end_j_) {}
+// _dense_space<1> * _dense_space<1> = _dense_space<2>?
+template <int DIM> struct _dense_space {
+	std::array<int, DIM> start, end;
+	static constexpr int dim = DIM;
 
-	bool operator!=(space2d rhs) const noexcept {
-		return rhs.start_i != start_i || rhs.start_j != start_j || rhs.end_i != end_i || rhs.end_j != end_j;
+	template <typename... argsT> _dense_space(const int _start, const int _end, argsT... args) {
+		static_assert(sizeof...(args) == (DIM - 1) * 2, "Missing constructor parameters for dense_space.");
+		start[0] = _start;
+		end[0] = _end;
+		init<DIM - 1>(std::forward<argsT>(args)...);
+	}
+
+	bool operator!=(const _dense_space<DIM> &rhs) const noexcept { return rhs.start != start || rhs.end != end; }
+
+  private:
+	template <int N, typename... argsT> inline void init(const int _start, const int _end, argsT... args) {
+		static_assert(sizeof...(args) == (N - 1) * 2, "Internal error. Something is broken with our constructor.");
+		start[DIM - N] = _start;
+		end[DIM - N] = _end;
 	}
 };
+
+// just a little helper
+template <typename... argsT> auto dense_space(argsT... args) {
+	return _dense_space<sizeof...(argsT) / 2>(std::forward<argsT>(args)...);
+}
 
 template <typename spaceT> struct _rm_order {
 	spaceT _space;
@@ -57,12 +73,12 @@ template <typename spaceT> struct _rm_order {
 	// TODO generalize it, only ok if spaceT === space2d
 	static void next(int &i, int &j, spaceT space) {
 		++i;
-		if (i >= space.end_i) {
-			i = space.start_i;
+		if (i >= space.end[0]) {
+			i = space.start[0];
 			++j;
-			if (j >= space.end_j) {
-				i = space.end_i;
-				j = space.end_j;
+			if (j >= space.end[1]) {
+				i = space.end[0];
+				j = space.end[1];
 			}
 		}
 	}
@@ -78,8 +94,8 @@ template <typename spaceT> struct _rm_order {
 	auto end() const noexcept {
 		// FIXME infere dimension from spaceT
 		auto temp = iteration<2, spaceT>(_space);
-		temp.i = _space.end_i;
-		temp.j = _space.end_j;
+		temp.i = _space.end[0];
+		temp.j = _space.end[1];
 		temp._order = next;
 
 		return temp;
@@ -87,23 +103,25 @@ template <typename spaceT> struct _rm_order {
 };
 
 // just a little helper
-template <typename T> _rm_order<T> rm_order(T instance) { return _rm_order<T>(instance); }
+template <typename T> auto rm_order(T &&instance) { return _rm_order<T>(std::forward<T>(instance)); }
 
 // TODO should allow to select the dimonsion used to partition
 template <typename spaceT> struct _static_partition : public spaceT {
 	_static_partition() = delete;
 
-	_static_partition(spaceT o) : spaceT(o) {
+	_static_partition(const int dim, spaceT o) : spaceT(o) {
 		int id = omp_get_thread_num();
 		int threads = omp_get_num_threads();
-		int size = spaceT::end_i - spaceT::start_i;
-		spaceT::start_i = (size / threads) * id;
-		if (id != threads - 1) spaceT::end_i = size / threads * (id + 1);
+		int size = spaceT::end[dim] - spaceT::start[dim];
+		spaceT::start[dim] = (size / threads) * id;
+		if (id != threads - 1) spaceT::end[dim] = size / threads * (id + 1);
 	}
 };
 
 // just a little helper
-template <typename T> _static_partition<T> static_partition(T instance) { return _static_partition<T>(instance); }
+template <typename T> auto static_partition(const int dim, T &&instance) {
+	return _static_partition<T>(dim, std::forward<T>(instance));
+}
 
 int main(int argc, char const *argv[]) {
 
@@ -112,7 +130,7 @@ int main(int argc, char const *argv[]) {
 #pragma omp parallel
 	{
 		int i, j;
-		for (auto iteration : rm_order(static_partition(space2d(1, 99, 1, 99)))) {
+		for (const auto &iteration : rm_order(static_partition(0, dense_space(1, 99, 1, 99)))) {
 			std::tie(i, j) = std::move(iteration);
 			arr1[i][j] = (arr2[i - 1][j] + arr2[i + 1][j] + arr2[i][j - 1] + arr2[i][j + 1]) / 4;
 		}
